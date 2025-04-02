d24303f412571ccd5b7022be20c3b6706aa3f1e0
MostafaVT <mostafaahmed96320@gmail.com>
2025-04-03 00:08:33 +0200

    feat: implement SCNN model architecture python and update notebook jupyter

---
+++ models/scnn.py
@@ -0,0 +1,36 @@
+import snntorch as snn
+import torch.nn as nn
+import torch.nn.functional as F
+
+beta = 0.5
+spike_grad = True
+batch_size = 128
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize layers
+        self.conv1 = nn.Conv2d(1, 12, 5)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.conv2 = nn.Conv2d(12, 64, 5)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.fc1 = nn.Linear(64*13*13, 6)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+
+    def forward(self, x):
+
+        # Initialize hidden states and outputs at t=0
+        mem1 = self.lif1.init_leaky()
+        mem2 = self.lif2.init_leaky()
+        mem3 = self.lif3.init_leaky()
+
+        cur1 = F.max_pool2d(self.conv1(x), 2)
+        spk1, mem1 = self.lif1(cur1, mem1)
+
+        cur2 = F.max_pool2d(self.conv2(spk1), 2)
+        spk2, mem2 = self.lif2(cur2, mem2)
+
+        cur3 = self.fc1(spk2.view(batch_size, -1))
+        spk3, mem3 = self.lif3(cur3, mem3)
+        return spk3, mem3
